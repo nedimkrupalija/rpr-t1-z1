09a6a7cda560e01cd57029f8dc4e15ff2ce348a9
nedimkrupalija <115478342+nedimkrupalija@users.noreply.github.com>
2022-10-17 19:31:29 +0200

    Add files via upload

---
+++ main i main.class/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+public static void main(String[] args){
+Scanner ulaz = new Scanner(System.in);
+System.out.println("Unesite prvi broj");
+int broj1 = ulaz.nextInt();
+System.out.println("Unesite drugi broj");
+int broj2 = ulaz.nextInt();
+System.out.println("Brojevi su: " + broj1 + " i " + broj2);
+}
+
+}
